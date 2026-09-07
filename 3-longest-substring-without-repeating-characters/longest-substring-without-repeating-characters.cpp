5d2e121043769e43a4af4d8b2acd147fd749f937
mdrehanansari420rd-hue <mdrehanansari420rd@gmail.com>
2026-09-07 16:28:03 +0530

    Time: 6 ms (82.24%) | Memory: 19.2 MB (50.17%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         int l=0,r=0,maxlen=0;
         vector<int>mpp(256,-1);
         while(r<n){
-            if(mpp[s[r]!=-1]){
+            if(mpp[s[r]]!=-1){
                 if(mpp[s[r]]>=l){
                     l=mpp[s[r]]+1;
                 }
