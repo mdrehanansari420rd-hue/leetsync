58fb721b2240f17f78e234ee4bd81d9243cd2b34
mdrehanansari420rd-hue <mdrehanansari420rd@gmail.com>
2026-09-07 15:57:51 +0530

    Time: 3 ms (91.58%) | Memory: 19 MB (54.55%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.size();
+        int l=0,r=0,maxlen=0;
+        vector<int>mpp(256,-1);
+        while(r<n){
+            if(mpp[s[r]!=-1]){
+                if(mpp[s[r]]>=l){
+                    l=mpp[s[r]]+1;
+                }
+            }
+            maxlen=max(maxlen,r-l+1);
+            mpp[s[r]]=r;
+            r++;
+        }
+        return maxlen;
+    }
+};
